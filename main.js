b7a90513024a7d38ec7531ccb28df6d57c8ebec6
CarlosRauda64 <rauda.carlos6432@gmail.com>
2024-08-27 20:27:55 -0600

    funcionalidad sobre promesas

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-//MANEJO DE EXCEPCIONES 
-const divide = (a,b) => {
-  if(b === 0)
-  {
-    throw new Error('Division entre cero no es posible')
-  }
-  return a/b
-}
-try{
-  //Aqui corre todo lo que esta bueno
-  console.log('resultado', divide(10,0))
-}catch{
-  //Aqui se manejan los errores
+const myPromise = new Promise((resolve, reject) =>{
+
+  setTimeout(() => {
+    let operationSucces = false
+    if (operationSucces){
+      resolve('operacion exitosa')
+    }else{
+      reject('operacion fallida')
+    }
+
+  }, 3000);
+
+})
+
+console.log(myPromise)
+//Resolver la promesa
+myPromise.then((message => {
+  console.log(message)
+})).catch((error) => {
   console.log(error)
-  console.log('Ocurrio un error dividiendo')
-}finally{
-  //Pase algo bueno o malo siempre se ejecuta
-  console.log('Siempre funca este volado')
-}
+})

af70b40045dd560b808593918c02dbaa6ef23d6a
CarlosRauda64 <rauda.carlos6432@gmail.com>
2024-08-28 19:13:53 -0600

    :feat: Uso de promesas

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-const myPromise = new Promise((resolve, reject) =>{
+//Rick and Morty API
+const endpoint = 'https://rickandmortyapi.com/api/character'
 
-  setTimeout(() => {
-    let operationSucces = false
-    if (operationSucces){
-      resolve('operacion exitosa')
-    }else{
-      reject('operacion fallida')
-    }
+fetch(endpoint)
+  .then( response => response.json() )
+  .then(data => {
+    const characters = data.results
+    const charactersContainer = document.getElementById('app')
+    //Map crea un nuevo array, dentro de el podemos recorrer todo
+    //el array, en este caso recorremos "characters" y "character"
+    //es la variable que almacena cada elemento del array
+    characters.map(character => {
+      const characterElement = document.createElement('div')
+      characterElement.innerHTML = `
+        <img src="${character.image}" alt="${character.name}-image">
+        <h3>Name: ${character.name}</h3>
+        <p>Status: ${character.status}</p>
+        <p>Gender: ${character.gender}</p>
+        <p>Species: ${character.species}</p>
+      `
+      charactersContainer.appendChild(characterElement)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+    const charactersContainer = document.getElementById('app')
+    charactersContainer.innerHTML = `<h1> API ERROR </h1>`
+  })
 
-  }, 3000);
-
-})
-
-console.log(myPromise)
-//Resolver la promesa
-myPromise.then((message => {
-  console.log(message)
-})).catch((error) => {
-  console.log(error)
-})

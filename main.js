516354711c79283e766c6a11b2148c630be55093
CarlosRauda64 <rauda.carlos6432@gmail.com>
2024-08-28 20:46:40 -0600

    :sparkles: Add API of Rick and Morty

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
+import './style.css'
+document.getElementById('app').innerHTML = `
+  <h1 class="title">RICK AND MORTY APP - CARLOS RAUDA<h1>
+  <section id="character-container"></section>
+`
+
 //Rick and Morty API
 const endpoint = 'https://rickandmortyapi.com/api/character'
 
-fetch(endpoint)
-  .then( response => response.json() )
-  .then(data => {
+//ASYNC-AWAIT
+const getCharacter = async () => {
+  try {
+    //Al agregar awit indicamos que esta operacion lleva tiempo
+    //Fetch se utiliza para realizar solicitudes HTTP de manera asíncrona
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    console.log(data.results)
     const characters = data.results
-    const charactersContainer = document.getElementById('app')
-    //Map crea un nuevo array, dentro de el podemos recorrer todo
-    //el array, en este caso recorremos "characters" y "character"
-    //es la variable que almacena cada elemento del array
+    const container = document.getElementById('character-container')
     characters.map(character => {
-      const characterElement = document.createElement('div')
-      characterElement.innerHTML = `
-        <img src="${character.image}" alt="${character.name}-image">
-        <h3>Name: ${character.name}</h3>
-        <p>Status: ${character.status}</p>
-        <p>Gender: ${character.gender}</p>
-        <p>Species: ${character.species}</p>
+      container.innerHTML += `
+        <div class="card">
+          <img src="${character.image}" alt="${character.name}-img">
+          <h3 class="card-title">Name: ${character.name}</h3>
+          <p class="card-species">Species: ${character.species}</p>
+          <p class="card-gender">Gender: ${character.gender}</p>
+          <p class="card-status">Status: ${character.status}</p>
+        </div>
       `
-      charactersContainer.appendChild(characterElement)
     })
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log(error)
-    const charactersContainer = document.getElementById('app')
-    charactersContainer.innerHTML = `<h1> API ERROR </h1>`
-  })
+  }
+}
 
+getCharacter()
